113613d2f04ada9f6820904489a71eeedcad518a
Nicolas Bloj <nicolasbloj@gmail.com>
2017-11-02 23:30:23 -0300

    firestore init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,16 @@
 import { Component } from '@angular/core';
+import { AngularFirestore } from 'angularfire2/firestore';
+import { Observable } from 'rxjs/Rx';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html'
 })
 export class AppComponent {
-  title = 'app';
+  chats: Observable<any[]>;
+  constructor(db: AngularFirestore) {
+    // nos ponemos pendiente de cambios del objeto items de firebase
+    this.chats = db.collection('chats').valueChanges();
+  }
 }
+
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,5 +4,13 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyCt3D7b6tw5oVxBhwOaJuTjEnNHC6j7e_0',
+    authDomain: 'fireapp-udemy.firebaseapp.com',
+    databaseURL: 'https://fireapp-udemy.firebaseio.com',
+    projectId: 'fireapp-udemy',
+    storageBucket: '',
+    messagingSenderId: '877373648815'
+  }
 };
